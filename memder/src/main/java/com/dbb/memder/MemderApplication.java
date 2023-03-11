0d76e4b1752b54dbc120a2e8062c2aef356c8f38
dengbingbing123 <2746670063@qq.com>
2023-03-11 17:48:20 +0800

    初始化项目

---
+++ memder/src/main/java/com/dbb/memder/MemderApplication.java
@@ -0,0 +1,13 @@
+package com.dbb.memder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemderApplication.class, args);
+    }
+
+}
---
+++ memder/src/test/java/com/dbb/memder/MemderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbb.memder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
